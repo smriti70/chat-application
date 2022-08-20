4c73607d8ecc816506c4e81a3e15fa371f24cb7b
Smriti Singh <smriti2815@gmail.com>
2022-08-20 15:21:36 +0530

    add message and location to dom

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,24 +1,68 @@
 const socket = io();
 
-document.querySelector('#chat-form').addEventListener('submit',(e)=>{
-    e.preventDefault();
-    let chatMessage = e.target.elements.message.value;
-    socket.emit('sendMessage',chatMessage);
+//Elements
+const $messageForm = document.querySelector('#chat-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages');
+
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
+
+socket.on('message',(message)=>{
+    console.log(message);
+    const html = Mustache.render(messageTemplate,{
+        message
+    });
+    $messages.insertAdjacentHTML('beforeend',html);
 });
 
-document.querySelector('#send-location').addEventListener('click',()=>{
+socket.on('locationMessage',(location)=>{
+    console.log(location);
+    const html = Mustache.render(locationMessageTemplate,{
+        location
+    });
+    $messages.insertAdjacentHTML('beforeend',html);
+});
+
+$messageForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    $messageFormButton.setAttribute('disabled','disabled');
+    
+    //disable
+    const chatMessage = e.target.elements.message.value;
+
+    socket.emit('sendMessage',chatMessage,(error)=>{
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
+        //enable
+        if(error){
+            return console.log("Error: ",error);
+        }
+        console.log("Message was sent!");
+    });
+});
+
+$sendLocationButton.addEventListener('click',()=>{
     if(!navigator.geolocation){
         return alert('Geolocation is not supported by your browser.');
     }
+
+    $sendLocationButton.setAttribute('disabled','disabled');
 
     navigator.geolocation.getCurrentPosition((position)=>{
         socket.emit('sendLocation',{
             latitude:position.coords.latitude,
             longitude:position.coords.longitude
-        })
+        },()=>{
+            console.log("Location shared!");
+        });
+        $sendLocationButton.removeAttribute('disabled');
     })
 })
 
-socket.on('message',(message)=>{
-    console.log(message);
-});
+
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -18,7 +19,14 @@ io.on('connection',(socket)=>{
     socket.broadcast.emit('message','A user has joined!');
     
     socket.emit('message',"Welcome!");
-    socket.on('sendMessage',(message)=>{
+    socket.on('sendMessage',(message,callback)=>{
+        const filter = new Filter();
+
+        if(filter.isProfane(message)){
+            return callback("Profanity is not allowed!");
+        }
+
+        callback("");
         io.emit('message',message);
     });
 
@@ -26,8 +34,9 @@ io.on('connection',(socket)=>{
         io.emit('message','A user has left!');
     });
 
-    socket.on('sendLocation',(location)=>{
-        io.emit('message',`https://google.com/maps?q=${location.latitude},${location.longitude}`);
+    socket.on('sendLocation',(location,callback)=>{
+        io.emit('locationMessage',`https://google.com/maps?q=${location.latitude},${location.longitude}`);
+        callback();
     });
 });
 
